8c5b8f1657edcc274676b026f12563c2112f5900
vagner bohm <vagnerbohm@pop-os.localdomain>
2023-07-20 09:56:08 -0300

    first commit

---
+++ src/main/java/com/vagnerbohm/products/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.vagnerbohm.products.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public void getAll(){
+
+    }
+}
